0478da59c0c22c0e996a40fbc86fd90025c4dbcd
Isabella Isaac Fernandes <isabella.isaacfernandes@gmail.com>
2019-10-17 21:58:25 -0300

    Sass e BEM

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var vez = 1;
+var vencedor = "";
+
 function allowDrop(ev) {
     ev.preventDefault();
 }
@@ -10,4 +13,41 @@ function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
+    verificarFimDeJogo();
+}
+
+function camposIguais(a, b, c) {
+    var campoA = $("#campo" + a);
+    var campoB = $("#campo" + b);
+    var campoC = $("#campo" + c);
+    var bgA = $("#campo" + a).children().attr('src');;
+    var bgB = $("#campo" + b).children().attr('src');;
+    var bgC = $("#campo" + c).children().attr('src');;
+    if ((bgA == bgB) &&
+        (bgB == bgC) &&
+        (bgA != undefined &&
+            bgA != "")) {
+
+        if (bgA.indexOf("img/bolinha.png") >= 0)
+            vencedor = "1";
+        else
+            vencedor = "2";
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function verificarFimDeJogo() {
+    if (camposIguais(1, 2, 3) ||
+        camposIguais(4, 5, 6) ||
+        camposIguais(7, 8, 9) ||
+        camposIguais(1, 4, 7) ||
+        camposIguais(2, 5, 8) ||
+        camposIguais(3, 6, 9) ||
+        camposIguais(1, 5, 9) ||
+        camposIguais(3, 5, 7)
+    ) {
+        $("#resultado").html("<h1>O jogador " + vencedor + "venceu! </h1>");
+    }
 }
